c694dd3efd0a801686753cbf749838a4cad818fc
Alice <sdflem+alice@gmail.com>
2018-11-07 21:02:06 +0000

    Added Alice page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'home', to: 'pages#home', as: 'home'
+  get 'alice', to: 'pages#alice', as: 'alice'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
